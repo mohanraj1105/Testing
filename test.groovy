410ce704a237fc934d1f31fb98f828a7e618f69b
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-07 21:11:54 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,7 +4,7 @@ pipeline{
             stage("test"){
                 steps{
                     script{
-                        sh "C:\Users\mp05\PycharmProjects\mohan"
+                        sh "../../PycharmProjects/mohan"
                         def val=pwd()
                         println(val)
                         }
