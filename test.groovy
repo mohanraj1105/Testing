67bc6188c61155b92a40f3075e28d802a59da973
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-08 22:27:43 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@ pipeline{
        stages{
             stage("test"){
                 steps{
-                    bat "python -m  robot.run --outputdir reports  C:/Users/mp05/PycharmProjects/mohan/robot_test/test.robot "                    
+                    bat "python -m  robot.run --outputdir reports  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
                     }
                 post{
                     always{
