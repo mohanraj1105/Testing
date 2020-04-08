93553a44ad0d03b3350b8ea779270bf51d6d857b
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-08 16:53:51 +0530

    Add files via upload

---
+++ test.groovy
@@ -0,0 +1,19 @@
+pipeline{
+    agent any{
+       stages{
+            stage("test"){
+                step{
+                    scripts{
+                        echo "hello world"
+                    }
+                }
+                post{
+                    always{
+                        archiveArtifacts  artifacts:  "**/*.xml",fingerprint: true
+                    }
+                }
+
+            }
+       }
+    }
+}
