e1c1f64ef4c05b7216c2b582cbdff4333b94fc8d
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-09 22:00:55 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -6,7 +6,9 @@ pipeline{
                     bat "python -m  robot.run --outputdir reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
                     script{
                         def num = BUILD_NUMBER
+                        def job = JOB_NAME
                         println(num)
+                        println(job)
                     }
                 }
                 post{
