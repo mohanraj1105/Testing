f7a2bb105befe6fb8360b8326f58d2b8b2f1ccb7
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-09 12:35:13 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,11 +4,11 @@ pipeline{
             stage("test"){
                 steps{
                     bat "python -m  robot.run --outputdir reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
-                    }
                     script{
                         def num = env.BUILD_NUMBER
                         println(num)
-                    }     
+                    }
+                }
                 post{
                     always{
                         robot "reports"
