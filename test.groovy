ecb78b4728682e148ab53028d7fe73e3f417bac6
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-12 20:57:43 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,6 +1,6 @@
 pipeline{
     agent any
-       stages{
+       stages{  
             stage("test"){
                 steps{
                     bat "python -m  robot.run --outputdir reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
@@ -14,6 +14,11 @@ pipeline{
                 post{
                     always{
                         robot "reports"
+                        copyArtifact    projectName:"${JOB_NAME}"
+                                        target: "test_1"
+                                        selector:lastSuccessful()
+                                        flatten:True
+                                        filter:"results/*.xml"
                          }
                     failure{
                         archiveArtifacts "**/*.png"
