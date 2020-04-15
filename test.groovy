c194175c012cabb453a98927bb43fc4782a7bbf6
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-15 19:04:40 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@ pipeline{
        stages{  
             stage("test"){
                 steps{
-                    bat "python -m  robot.run --outputdir reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
+                    bat "python -m  robot.run --outputdir reports -n *open* --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
                     script{
                         def num = BUILD_NUMBER
                         def job = JOB_NAME
