0bcf47d3bc6fcc2514f9bc79f01fc9a54ddab3d7
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-19 23:00:44 +0530

    Update test_file_params.groovy

--- test_file_params.groovy
+++ test_file_params.groovy
@@ -1,8 +1,11 @@
+def triggering = "H/4 * * * *"
 pipeline{
     agent any
     parameters{
         string(name:'env' , defaultValue:'simu', description:'')
-    }
+       }
+    triggers{
+        cron(triggering)
     stages{        
         stage('test'){
             steps{
