7ababd4e913de5b96ecc525f783d6c44b44ceab2
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-04-23 21:13:19 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@ pipeline{
        stages{  
             stage("test"){
                 steps{
-                    bat "python -m  robot.run --outputdir reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
+                    bat "python -m  robot.run  -d reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/ "                    
                     script{
                         def num = BUILD_NUMBER
                         def job = JOB_NAME
@@ -15,14 +15,13 @@ pipeline{
                 post{
                     always{
                         robot "reports"
-                        bat "7z -ttar a dummy reports -so | 7z -si -tgzip a x.tgz"
-                         }
+           
+                        }
                     failure{
                         archiveArtifacts "**/*.png"
+                        archieveArtifacts "**/*.tar.gz"
                         }
-                    success{
-                        archieveArtifacts "**/*.tar"
-                        }
+                   
                     }
                 }
             }
