be29a46a93a8b899737fae73b944c833c3a89888
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-06-05 09:55:58 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,9 +4,9 @@ pipeline{
             stage("test"){
                 steps{                   
                     script{
+                        env.RERUN = 'TRUE'
                         catchError(buildResult:'SUCCESS',stageResult:'FAILURE'){
                         bat "python -m  robot.run  -d reports --splitlog  C:/Users/mp05/PycharmProjects/mohan/robot_test/"
-                        env.RERUN = 'TRUE'
                         }
                         def num = BUILD_URL
                         def job = JOB_NAME
