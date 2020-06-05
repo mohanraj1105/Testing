7f2863b595ea0703452cbfe599a3afad201d98f2
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-06-05 14:09:47 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -44,11 +44,11 @@ pipeline{
                    script{
                    bat "python -m robot.run --rerunfailed reports/output.xml --output output_2.xml --log log_2.html --report report_2.html -d reports C:/Users/mp05/PycharmProjects/mohan/robot_test/"
                         }
-                   script{
-                     bat "python -m robot.rebot --merge reports/output_1.xml reports/output_2.xml -d reports"
-                       }
                }
                post{
+                  script{
+                     bat "python -m robot.rebot --merge reports/output_1.xml reports/output_2.xml -d reports"
+                        }
                    always{
                        robot outputPath : 'reports',
                        logFileName : 'log.html',
