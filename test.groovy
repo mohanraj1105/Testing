93a7297395f7c445df9ffe23935af7ddfab497f5
mohanraj1105 <63236690+mohanraj1105@users.noreply.github.com>
2020-06-05 14:17:09 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -56,7 +56,7 @@ pipeline{
                    reportFileName : 'report.html'    
                    
                     }
-               
+               }
                        
            }
        }
